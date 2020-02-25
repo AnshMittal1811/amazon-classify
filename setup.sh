2ce118821629d93aad928fa9547023968a13b366
akshatgoel92 <akshatgoel92@hotmail.com>
2020-02-25 18:02:36 +0530

    Add shell script to set up deep learning software on Ubuntu 18.04

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+python3.6 -m venv dl-scratch
+git clone https://github.com/sociometrik/planet-rep.git
+cd planet-rep
+source ./../dl-scratch/bin/activate
+pip --version
+pip3 install --upgrade pip==19.0.0
+pip3 install -r requirements.txt
